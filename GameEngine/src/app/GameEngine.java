6326610d1980c5e981167e73d7748867fc9d864c
Caiua Gomes de Franca <caiua.franca@ale.com.br>
2019-10-23 17:42:36 -0300

    GameEngine inciando em java

---
+++ GameEngine/src/app/GameEngine.java
@@ -0,0 +1,44 @@
+package app;
+
+public class GameEngine implements Runnable {
+
+    public boolean isRunning;
+    private Thread thread;
+
+    public static void main(String[] args) throws Exception {
+        GameEngine game = new GameEngine();
+        game.start();
+    }
+
+    public synchronized void start() {
+        isRunning = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public void tick() {
+        //Atualizar os Jogos
+        System.out.println("Tick");
+    }
+
+    public void render() {
+        //Renderizar os Jogos
+        System.out.println("Render");
+    }
+
+    @Override
+    public void run() {
+        while (isRunning) {
+            tick();
+            render();
+            /*
+            try {
+                Thread.sleep(1000/60);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            */
+        }
+
+    }
+}
